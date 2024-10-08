f7579b0c25e8d6d218a039359347c22414a6eea8
ritik <ritikdhimanup8@gmail.com>
2024-10-09 00:09:34 +0530

    api issue fix

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_DOMAIN: string = 'http://localhost:3000'
+export const SERVER_DOMAIN: string = 'https://67057a26ebf7dc20171b13c4--imaginative-hotteok-710e80.netlify.app/'
 
 export const isValidEmailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
